bf22eb69df14db38320361e2095ef80af63e57de
TT-392 <tt-392@protonmail.com>
2020-09-23 23:27:59 +0200

    made sl function nicer

--- src/main.c
+++ src/main.c
@@ -16,36 +16,12 @@
 _Bool toggle = 0;
 
 int time = 1;
-void TIMER0_IRQHandler(void) {
-//    display_backlight(255*toggle);
-    time++;
-
-    nrf_timer_event_clear(NRF_TIMER0, NRF_TIMER_EVENT_COMPARE0);
-    nrf_timer_task_trigger(NRF_TIMER0, NRF_TIMER_TASK_CLEAR);
-}
 
 
 
 int main(void) {
-    // Irq setup
-    NVIC_SetPriority(TIMER0_IRQn, 15); // Lowes priority
-    NVIC_ClearPendingIRQ(TIMER0_IRQn);
-    NVIC_EnableIRQ(TIMER0_IRQn);
-
-
-    nrf_timer_mode_set(NRF_TIMER0,NRF_TIMER_MODE_TIMER);
-    nrf_timer_bit_width_set(NRF_TIMER0, NRF_TIMER_BIT_WIDTH_32);
-    nrf_timer_frequency_set(NRF_TIMER0, NRF_TIMER_FREQ_1MHz);
-    nrf_timer_cc_write(NRF_TIMER0, NRF_TIMER_CC_CHANNEL0, 1000000/8);
-
-    nrf_timer_int_enable(NRF_TIMER0,NRF_TIMER_INT_COMPARE0_MASK );
-    nrf_timer_task_trigger(NRF_TIMER0, NRF_TIMER_TASK_START);
-
-
 
     battery_init();
-
-
 
     bool osRunning = 1;
 
@@ -81,21 +57,25 @@ int main(void) {
 
     int lastTime = 0;
     int i = 0;
+
+    bool running = 1;
+    while (running) {
+        wdt_feed();
+        if (sl_nextFrameReady) {
+            running = !sl(65, 0xffff, 0x0000);
+        }
+    }
+
+
     while(osRunning) {
         battery_percent(200,0,0xffff,0x0000);
        // heart_loop();
         wdt_feed();
 
-//        sl_static(40, 90, 0xffff, 0x0000);
-        if (lastTime != time) {
-            i++;
-            if (i >= (30+22)) { 
-                i = 0;
-            }
-            sl(65,i,0xffff,0x0000);
-            lastTime = time;
-        }
 
+        if (sl_nextFrameReady) {
+           sl(65, 0xffff, 0x0000);
+        }
 
 
         nrf_delay_us(1);
--- src/modules/steamLocomotive.c
+++ src/modules/steamLocomotive.c
@@ -1,5 +1,15 @@
 #include "../display_print.h"
 
+volatile bool sl_nextFrameReady = 1;
+
+void TIMER0_IRQHandler(void) {
+    sl_nextFrameReady = 1;
+
+    nrf_timer_event_clear(NRF_TIMER0, NRF_TIMER_EVENT_COMPARE0);
+    nrf_timer_task_trigger(NRF_TIMER0, NRF_TIMER_TASK_CLEAR);
+}
+
+
 void sl_static(int x, int y, uint16_t color_text, uint16_t color_bg) {
     char* train[6] = {
     "      ++      +------", 
@@ -17,7 +27,32 @@ void sl_static(int x, int y, uint16_t color_text, uint16_t color_bg) {
     }
 }
 
-void sl (int y, int time, uint16_t color_text, uint16_t color_bg) {
+
+
+int sl(int y, uint16_t color_text, uint16_t color_bg) {
+    sl_nextFrameReady = 0;
+    static int time = 0;
+
+    static bool firstframe = 1;
+    if (firstframe) {
+        // Irq setup
+        NVIC_SetPriority(TIMER0_IRQn, 15); // Lowes priority
+        NVIC_ClearPendingIRQ(TIMER0_IRQn);
+        NVIC_EnableIRQ(TIMER0_IRQn);
+
+
+        nrf_timer_mode_set(NRF_TIMER0,NRF_TIMER_MODE_TIMER);
+        nrf_timer_bit_width_set(NRF_TIMER0, NRF_TIMER_BIT_WIDTH_32);
+        nrf_timer_frequency_set(NRF_TIMER0, NRF_TIMER_FREQ_1MHz);
+        nrf_timer_cc_write(NRF_TIMER0, NRF_TIMER_CC_CHANNEL0, 1000000/8);
+
+        nrf_timer_int_enable(NRF_TIMER0,NRF_TIMER_INT_COMPARE0_MASK );
+        nrf_timer_task_trigger(NRF_TIMER0, NRF_TIMER_TASK_START);
+
+        firstframe = 0;
+    }
+
+
     char* trains[6][6] = {{
     "      ++      +------ ", 
     "      ||      |+-+ | ",
@@ -63,27 +98,30 @@ void sl (int y, int time, uint16_t color_text, uint16_t color_bg) {
     }};
 
 
-
-
-    
-    for (int i = 0; i < 6; i++) {
+    for (int line = 0; line < 6; line++) {
         int x = (30-time)*8;
-        int charY = y + i*16;
-        char* string = trains[(time/2)%6][i];
+        int charY = y + line*16;
+        char* string = trains[(time/2)%6][line];
         _Bool terminated = 0;
-        int j = 1;
+        int character = 1;
 
         while (!terminated) {
-            if (string[j] == '\0') {
+            if (string[character] == '\0') {
                 terminated = 1;
             } else {
-                if ((x + j*8) < 240 && (x + j*8) >= 0) {
-                    if ((i > 3 && j > 6) || string[j-1] != string[j]) {
-                        drawChar (x + j*8, charY, string[j], color_text, color_bg);
-                    }
+                if (((x + character*8) < 240 && (x + character*8) >= 0) // char out of display bounds
+                && ((line > 3 && character > 6) || string[character-1] != string[character])) { // optimization
+                    drawChar (x + character*8, charY, string[character], color_text, color_bg);
                 }
-                j++;
+                character++;
             }
         }
+    }
+    time++;
+    if (time >= (30+22)) {
+        time = 0;
+        return 1;
+    } else {
+        return 0;
     }
 }
