4a29eb686d16f577787aa788e05cfea3f5b819e7
TT-392 <tt-392@protonmail.com>
2020-09-24 02:22:34 +0200

    started work on processor

--- src/main.c
+++ src/main.c
@@ -10,7 +10,8 @@
 #include "steamLocomotive.h"
 #include "modules/battery.c"
 //#include "modules/keyboard.c"
-#include "frame.c"
+//#include "frame.c"
+#include "test.c"
 
 _Bool toggle = 0;
 
@@ -19,36 +20,40 @@ int time = 1;
 
 
 int main(void) {
-
     battery_init();
-
     bool osRunning = 1;
 
-    //osRunning = !nrf_sdh_enable_request();
-//    sd_softdevice_enable();
     nrf_gpio_cfg_output(10);
     nrf_gpio_pin_write(10,0);
     nrf_delay_ms(20);
     nrf_gpio_pin_write(10,1);
     nrf_delay_ms(20);
 
-
     display_backlight(255);
     display_init();
 
-///    drawmono(0, 0, 239, 239, frame, 0x0000, 0xffff);
-
-
-    
- //       writesquare(0, 0, 240, 240, 0xffff);
     writesquare(0, 0, 240, 240, 0x0000);
 
-    nrf_delay_ms(2000);
+
+
+
+    uint8_t frame[originalFrameLength] = {};
+    _Bool currentColor = 0;
+    uint16_t binAddress = 0;
+    for (int i = 0; i < compressedLength; i++) {
+        for (int j = 0; j < compressedFrame[i]; j++) {
+            frame[binAddress / 8] |= currentColor << (binAddress % 8);
+            binAddress++;
+        }
+        currentColor = !currentColor;
+    }
+
+
+   drawmono(0, 0, 239, 239, frame, 0x0000, 0xffff);
+
+
 
     nrf_gpio_cfg_input(19, NRF_GPIO_PIN_NOPULL);
-
-
-
 
     bool backlight = 1;
     bool lastButState = 0;
@@ -58,35 +63,23 @@ int main(void) {
     int i = 0;
 
     bool running = 1;
-    while (running) {
-        wdt_feed();
-        if (sl_nextFrameReady) {
-            running = !sl(65, 0xffff, 0x0000);
-        }
-    }
-
+//    while (running) {
+//        wdt_feed();
+//        if (sl_nextFrameReady) {
+//            running = !sl(65, 0xffff, 0x0000);
+//        }
+//    }
 
     while(osRunning) {
-        battery_percent(200,0,0xffff,0x0000);
-       // heart_loop();
+//        battery_percent(200,0,0xffff,0x0000);
         wdt_feed();
 
-
-        if (sl_nextFrameReady) {
-           sl(65, 0xffff, 0x0000);
-        }
-
-
-        nrf_delay_us(1);
+ //       if (sl_nextFrameReady) {
+ //          sl(65, 0xffff, 0x0000);
+ //       }
 
         if (nrf_gpio_pin_read(13)) {
             osRunning = 0;
         }
-
-        
-        
-
-
     }
-
 }
