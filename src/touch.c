8a437cc8230fbeeffed76f643e9e5f40ac66f398
TT-392 <tt-392@protonmail.com>
2020-11-16 20:10:05 +0100

    flashed out the non blocking touch driver

--- src/touch.c
+++ src/touch.c
@@ -4,24 +4,39 @@
 #include "nrf_gpio.h"
 #include "display_defines.h"
 #include "display.h"
+#include "semihost.h"
 
 #define TOUCH_I2C_DEVICE (0x15)
 
 #define PIN_SCL        (7)
 #define PIN_SDA        (6)
 
-volatile static bool stopped = 0;
 void SPIM1_SPIS1_TWIM1_TWIS1_SPI1_TWI1_IRQHandler(void) {
-    NRF_TWIM1->EVENTS_STOPPED = 0;
-    stopped = 1;
+    if (NRF_TWIM1->EVENTS_TXSTARTED) {
+        NRF_TWIM1->EVENTS_TXSTARTED = 0;
+        NRF_TIMER1->TASKS_CLEAR = 1;
+    }
 }
 
-static bool toggle_backlight = 1;
+
+// timer 1 detects if twim is taking too long
 void TIMER1_IRQHandler(void) {
-    display_backlight(toggle_backlight);
-    //   toggle_backlight = !toggle_backlight;
-    NRF_TIMER1->TASKS_CLEAR = 1;
     NRF_TIMER1->EVENTS_COMPARE[0] = 0;
+
+    // fix i2c when it gets stuck
+    
+    // disable i2c
+    uint32_t twi_state = NRF_TWIM1->ENABLE;
+    NRF_TWIM1->ENABLE = TWIM_ENABLE_ENABLE_Disabled << TWI_ENABLE_ENABLE_Pos;
+
+    // force pins back
+    nrf_gpio_cfg_input(PIN_SCL, NRF_GPIO_PIN_PULLUP);
+    nrf_gpio_cfg_input(PIN_SDA, NRF_GPIO_PIN_PULLUP);
+
+    NRF_TWIM1->ENABLE = twi_state;
+
+    // restart i2c
+    NRF_TWIM1->TASKS_STARTTX = 1;
 }
 
 struct touchPoints {
@@ -32,20 +47,13 @@ struct touchPoints {
     uint8_t pressure;
 };
 
-#define reset_events() NRF_TWIM1->EVENTS_LASTRX = 0;\
-      NRF_TWIM1->EVENTS_STOPPED = 0;\
-      NRF_TWIM1->EVENTS_LASTTX = 0;\
-      NRF_TWIM1->EVENTS_ERROR = 0;\
-      NRF_TWIM1->EVENTS_RXSTARTED = 0;\
-      NRF_TWIM1->EVENTS_SUSPENDED = 0;\
-      NRF_TWIM1->EVENTS_TXSTARTED = 0;\
-
 static volatile uint8_t touch_data[8];
 static volatile uint8_t registerNr = 0;
 
-int touch_init() {
-    static int once = 1;
+int touch_init() { 
+//    semihost_print("enabling touch\n", 15);
 
+    // send a reset
     nrf_gpio_cfg_output(10);
     nrf_gpio_pin_write(10,1);
     nrf_delay_ms(50);
@@ -54,11 +62,9 @@ int touch_init() {
     nrf_gpio_pin_write(10,1);
     nrf_delay_ms(50);
 
-    nrf_gpio_cfg_output(16);
-    nrf_gpio_pin_write(16,1);
 
     // init i2c // 
-    // disable i2c
+    
     NRF_TWIM1->ENABLE = TWIM_ENABLE_ENABLE_Disabled << TWI_ENABLE_ENABLE_Pos;
 
 
@@ -70,17 +76,18 @@ int touch_init() {
     NRF_TWIM1->PSEL.SCL = PIN_SCL;
     NRF_TWIM1->PSEL.SDA = PIN_SDA;
 
+    // setup interrupts
     NRF_TWIM1->INTEN = 0;
-    NRF_TWIM1->INTENSET = 1 << 1;
+    NRF_TWIM1->INTENSET = /*1 << 1 |*/ 1 << 20;
 
     NVIC_SetPriority(SPIM1_SPIS1_TWIM1_TWIS1_SPI1_TWI1_IRQn, 2);
     NVIC_ClearPendingIRQ(SPIM1_SPIS1_TWIM1_TWIS1_SPI1_TWI1_IRQn);
     NVIC_EnableIRQ(SPIM1_SPIS1_TWIM1_TWIS1_SPI1_TWI1_IRQn);
 
-    reset_events();
     NRF_TWIM1->ENABLE = (TWIM_ENABLE_ENABLE_Enabled << TWIM_ENABLE_ENABLE_Pos);
 
 
+    // setup timer which detects if twim gets stuck
     NRF_TIMER1->MODE = 0 << TIMER_MODE_MODE_Pos; // timer mode
     NRF_TIMER1->BITMODE = 0 << TIMER_BITMODE_BITMODE_Pos; // 16 bit
     NRF_TIMER1->PRESCALER = 0xf << TIMER_PRESCALER_PRESCALER_Pos; // 16 MHz
@@ -92,44 +99,21 @@ int touch_init() {
     NVIC_EnableIRQ(TIMER1_IRQn);
     NRF_TIMER1->TASKS_START = 1;
 
-    // enabled shorts: LASTTX_SUSPEND, LASTRX_STOP
-    //         //
-    nrf_gpio_pin_write(16,1);
-    reset_events();
+
     // setup tx //
     NRF_TWIM1->ADDRESS = TOUCH_I2C_DEVICE;
     NRF_TWIM1->TXD.PTR = (uint32_t)&registerNr;
     NRF_TWIM1->TXD.MAXCNT = 1;
     NRF_TWIM1->TASKS_RESUME = 0x1UL;
-    NRF_TWIM1->SHORTS = 1 << 7 | 1 << 12;
+    NRF_TWIM1->SHORTS = 1 << 7 | /*1 << 12*/ 1 << 10;
     //          //
 
-    reset_events();
     // setup rx //
     NRF_TWIM1->ADDRESS = TOUCH_I2C_DEVICE;
     NRF_TWIM1->TASKS_RESUME = 0x1UL;
     NRF_TWIM1->RXD.PTR = (uint32_t)touch_data;
     NRF_TWIM1->RXD.MAXCNT = 8;
     //          //
-    NRF_TWIM1->TASKS_STARTTX = 1;
-
-    // counter = 0;
-    int counter = 0;
-    while (!stopped){
-        counter++;
-        if (counter > 500000) {
-            static int errcount = 0;
-            if (errcount >= 1) {
-                //    nrf_gpio_pin_write(16,0);
-                counter = 0;
-            } else {
-                return 1;
-            }
-            errcount++;
-        }
-    }
-    stopped = 0;
-
 
 
     return 0;
@@ -137,30 +121,14 @@ int touch_init() {
 
 
 int touch_refresh(struct touchPoints* touchPoint) {
-
     static bool once = 1;
     if (once) {
         touch_init();
+        NRF_TWIM1->TASKS_STARTTX = 1;
         once = 0;
     }
 
-
-
-
-    NRF_TWIM1->TASKS_STARTTX = 1;
-
-    int counter = 0;
-    while (!stopped){
-        counter++;
-        if (counter > 500000) {
-            static int errcount = 0;
-            return 1;
-            errcount++;
-        }
-
-    }
-    stopped = 0;
-
+    //semihost_print("reading touch\n", 14);
 
     touchPoint->gesture = touch_data[1];
     touchPoint->event = touch_data[3] & 0xf;
@@ -168,13 +136,13 @@ int touch_refresh(struct touchPoints* touchPoint) {
     touchPoint->touchY = touch_data[6];
     touchPoint->pressure = touch_data[7];
 
-    drawNumber(130, 20, touch_data[1], 0xffff, 0x0000, 3, 0);
-    drawNumber(130, 40, touch_data[2], 0xffff, 0x0000, 3, 0);
-    drawNumber(130, 60, touch_data[3], 0xffff, 0x0000, 3, 0);
-    drawNumber(130, 80, touch_data[4], 0xffff, 0x0000, 3, 0);
-    drawNumber(130, 100, touch_data[5], 0xffff, 0x0000, 3, 0);
+//    drawNumber(130, 20, touch_data[1], 0xffff, 0x0000, 3, 0);
+//    drawNumber(130, 40, touch_data[2], 0xffff, 0x0000, 3, 0);
+//    drawNumber(130, 60, touch_data[3], 0xffff, 0x0000, 3, 0);
+//    drawNumber(130, 80, touch_data[4], 0xffff, 0x0000, 3, 0);
+//    drawNumber(130, 100, touch_data[5], 0xffff, 0x0000, 3, 0);
     drawNumber(130, 120, touch_data[6], 0xffff, 0x0000, 3, 0);
-    drawNumber(130, 140, touch_data[7], 0xffff, 0x0000, 3, 0);
+//    drawNumber(130, 140, touch_data[7], 0xffff, 0x0000, 3, 0);
 
     return 0;
 
