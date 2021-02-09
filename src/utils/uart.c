24b7e3da88085864e2c25cf21beca8f18e687248
TT-392 <tt-392@protonmail.com>
2021-02-09 01:02:12 +0100

    added uart debugging

---
+++ src/utils/uart.c
@@ -0,0 +1,44 @@
+#include "nrf.h"
+#include "nrf_delay.h"
+#include "nrf_gpio.h"
+
+#define PIN_TXD (11)
+
+void uart_send(char* data) {
+    int i = 0;
+    while(data[i] != '\0') {
+        i++;
+    }
+    NRF_UARTE0->TXD.MAXCNT = i;//sizeof(data);
+    NRF_UARTE0->TXD.PTR = (uint32_t)&data[0];
+    NRF_UARTE0->TASKS_STARTTX = 1;
+
+    while (NRF_UARTE0->EVENTS_ENDTX == 0);
+    NRF_UARTE0->EVENTS_ENDTX = 0;
+
+    NRF_UARTE0->TASKS_STOPTX = 1;
+    while (NRF_UARTE0->EVENTS_TXSTOPPED == 0);
+
+    NRF_UARTE0->EVENTS_TXSTOPPED = 0;
+
+    static int x = 0;
+    if (x == 0)
+        nrf_delay_us(1);
+    x = 1;
+}
+
+void uart_init() { 
+    nrf_gpio_cfg_output(PIN_TXD);	
+
+
+    NRF_UARTE0->CONFIG = (0 << UARTE_CONFIG_HWFC_Pos) |
+        (0 << UARTE_CONFIG_PARITY_Pos);
+
+    NRF_UARTE0->BAUDRATE = UARTE_BAUDRATE_BAUDRATE_Baud9600 << UARTE_BAUDRATE_BAUDRATE_Pos;
+    NRF_UARTE0->PSEL.TXD = PIN_TXD;
+    NRF_UARTE0->ENABLE = UARTE_ENABLE_ENABLE_Enabled << UARTE_ENABLE_ENABLE_Pos;
+
+    int x = 0;
+
+}
+
---
+++ src/utils/uart.h
@@ -0,0 +1,5 @@
+#pragma once
+
+void uart_init();
+
+void uart_send(char* data);
