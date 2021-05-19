5b125657dda228825ce5089cca58aa30c455d425
TT-392 <tt-392@protonmail.com>
2021-05-19 16:06:49 +0200

    put spinor stuff into functions

--- src/drivers/flash.c
+++ src/drivers/flash.c
@@ -1,4 +1,5 @@
 #include <nrf.h>
+#include <string.h>
 #include <stdint.h>
 #include <nrf_gpio.h>
 #include "flash.h"
@@ -14,6 +15,12 @@ static void spiWrite(uint8_t *data, int length) {
     while(NRF_SPIM0->EVENTS_END == 0) __NOP();
 }
 
+static void spiWriteByte(uint8_t byte) {
+    nrf_gpio_pin_write(FLASH_CS,0);
+    spiWrite(&byte, 1);
+    nrf_gpio_pin_write(FLASH_CS,1);
+}
+
 static void spiRead(volatile uint8_t *data, int length) {
     NRF_SPIM0->RXD.MAXCNT = length;
     NRF_SPIM0->RXD.PTR = (uint32_t)data;
@@ -24,95 +31,74 @@ static void spiRead(volatile uint8_t *data, int length) {
     NRF_SPIM0->RXD.MAXCNT = 0;
 }
 
-uint16_t checkStatus() {
+uint8_t checkStatus() {
     nrf_gpio_pin_write(FLASH_CS,0);
-    uint8_t data[2] = {0,0};
-    uint8_t RDSR[] = {0x05};
-    spiWrite(RDSR, sizeof(RDSR));
-
-    spiRead(data, 2);
+    uint8_t data;
+    uint8_t cmd = FLASH_RDSR;
+    spiWrite(&cmd, 1);
+    spiRead(&data, 1);
     nrf_gpio_pin_write(FLASH_CS,1);
+    return (data);
+}
 
-    return (data[0] + (data[1] << 8));
+void spiflash_sector_erase(int addr) {
+    nrf_gpio_pin_write(FLASH_CS,0);
+    uint8_t cmd[] = {FLASH_SE, addr >> 16, addr >> 8, addr};
+    spiWrite(cmd, 4);
+    nrf_gpio_pin_write(FLASH_CS,1);
+    while (checkStatus() & 1);
+}
+
+void spiflash_write_data(int addr, uint8_t* data, int length) {
+    nrf_gpio_pin_write(FLASH_CS,0);
+    uint8_t cmd[length + 4];
+    cmd[0] = FLASH_PP; cmd[1] = addr >> 16; cmd[2] = addr >> 8; cmd[3] = addr;
+    memcpy(&cmd[4], data, length);
+    spiWrite(cmd, sizeof(cmd));
+    nrf_gpio_pin_write(FLASH_CS,1);
+    while (checkStatus() & 1); // potential optimization here, potentially unneccesary idle time
+}
+
+void spiflash_read_data(int addr, uint8_t* data, int length) {
+    nrf_gpio_pin_write(FLASH_CS,0);
+    uint8_t cmd[] = {FLASH_READ, addr >> 16, addr >> 8, addr};
+    spiWrite(cmd, sizeof(cmd));
+    spiRead(data, length);
+    nrf_gpio_pin_write(FLASH_CS,1);
 }
 
 void spiflash() {
-
-    //spinor_word_write(0, 69);
-
     nrf_gpio_cfg_output(FLASH_CS);
     nrf_gpio_pin_write(FLASH_CS,1);
 
-
     // init
-    nrf_gpio_pin_write(FLASH_CS,0);
-    uint8_t RDP[] = {0xab};
-    spiWrite(RDP, sizeof(RDP));
-    nrf_gpio_pin_write(FLASH_CS,1);
-    nrf_delay_ms(1000);
+    spiWriteByte(FLASH_RDI);
+    //nrf_delay_ms(1000);
     /////////
     
     // write enable
-    nrf_gpio_pin_write(FLASH_CS,0);
-    uint8_t WREN[] = {0x06};
-    spiWrite(WREN, sizeof(WREN));
-    nrf_gpio_pin_write(FLASH_CS,1);
+    spiWriteByte(FLASH_WREN);
     /////////
-    nrf_delay_ms(100);
 
     // sector erase
-    nrf_gpio_pin_write(FLASH_CS,0);
     int addr = 0;
-    uint8_t erasecmd[] = {FLASH_SE, addr >> 16, addr >> 8, addr};
-    spiWrite(erasecmd, sizeof(erasecmd));
-    nrf_gpio_pin_write(FLASH_CS,1);
+    spiflash_sector_erase(addr);
     /////////
-    nrf_delay_ms(100);
 
 
     // write enable
-    nrf_gpio_pin_write(FLASH_CS,0);
-    spiWrite(WREN, sizeof(WREN));
-    nrf_gpio_pin_write(FLASH_CS,1);
+    spiWriteByte(FLASH_WREN);
     /////////
-    nrf_delay_ms(100);
 
     // write data
-    nrf_gpio_pin_write(FLASH_CS,0);
-    uint8_t cmd[] = {FLASH_PP, addr >> 16, addr >> 8, addr, 69, 69, 69};
-    spiWrite(cmd, sizeof(cmd));
-    nrf_gpio_pin_write(FLASH_CS,1);
+    uint8_t writedata[4] = {0x00,0x00,0xaa,0x55};
+    spiflash_write_data(addr, writedata, 4);
     /////////
     
-   // volatile uint16_t status;
-   // status = checkStatus();
-    nrf_delay_ms(100);
-   // status = checkStatus();
-
     // read data
-    nrf_gpio_pin_write(FLASH_CS,0);
-    uint8_t readcmd[] = {FLASH_READ, addr >> 16, addr >> 8, addr};
-    volatile uint8_t data[10] = {0};
-    spiWrite(readcmd, sizeof(readcmd));
-    spiRead(data, 10);
-    nrf_gpio_pin_write(FLASH_CS,1);
+    uint8_t data[10];
+    spiflash_read_data(0, data, 10);
     /////////
-    nrf_delay_ms(100);
-
-
-    /*
-
-    nrf_gpio_pin_write(FLASH_CS,0);
-    uint8_t pageRead[4] = {FLASH_RDID, 0, 0, 0};
-    spiWrite(pageRead, 1);
-
-    volatile uint8_t data[10] = {0, 1, 2};
-
-    spiRead(data, 3);
-
-    nrf_gpio_pin_write(FLASH_CS,1);
-    */
-
     
 
     while(1);
--- src/drivers/flash.h
+++ src/drivers/flash.h
@@ -6,5 +6,7 @@
 #define FLASH_SE 0x20
 #define FLASH_READ 0x03
 #define FLASH_RDID 0x9F
+#define FLASH_RDSR 0x05
+#define FLASH_RDI 0xab
 
 void spiflash();
