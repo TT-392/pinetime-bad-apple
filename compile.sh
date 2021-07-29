aed6bafbe54aca8b5dc8b868bca69f3f2c336312
TT-392 <tt-392@protonmail.com>
2021-07-29 18:11:16 +0200

    added ota zip package creation to compile.sh

--- compile.sh
+++ compile.sh
@@ -16,12 +16,24 @@ then
 elif [ "$1" = "core_ota" ];
 then
     make -f Makefile_ota OUTFILENAME=bad_apple_ota
-elif [ "$1" = "spiflash_ota" ];
+
+    mkdir -p _build/ota
+    cd Adafruit_nRF52_nrfutil
+    #so, like, why does this work??
+    python3 -m nordicsemi dfu genpkg --dev-type 0x0052 --application ../_build/bad_apple_ota.hex ../_build/ota/bad_apple_ota.zip
+    cd -
+    elif [ "$1" = "spiflash_ota" ];
 then
     for f in src/bad_apple/resources/video*; do
         fileNr=${f#src/bad_apple/resources/video_}
         fileNr=${fileNr%\.h}
         echo $fileNr
         make OUTFILENAME=bad_apple_flash_ota_$fileNr EXTRAFLAGS=-DSPIFLASH=$fileNr
+
+        mkdir -p _build/ota
+        cd Adafruit_nRF52_nrfutil
+        #so, like, why does this work??
+        python3 -m nordicsemi dfu genpkg --dev-type 0x0052 --application ../_build/bad_apple_flash_ota_$fileNr.hex ../_build/ota/bad_apple_flash_ota$fileNr.zip
+        cd -
     done
 fi
