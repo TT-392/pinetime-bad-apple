514b22748fab0e9ef9a62515822353fd8d263f5c
TT-392 <tt-392@protonmail.com>
2021-07-31 03:00:05 +0200

    removed old stuff

--- make_ble.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-make -f Makefile_ota
-cd Adafruit_nRF52_nrfutil
-#so, like, why does this work??
-python3 -m nordicsemi dfu genpkg --dev-type 0x0052 --application ../_build/nrf52832_xxaa.hex ../_build/pine.zip
-cd -
